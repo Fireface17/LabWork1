6c79a2b801f8cfceea184891f8470ed66153e1e0
Tamerlan <103945044+Fireface17@users.noreply.github.com>
2022-10-28 22:23:53 +0600

    V2DataCollections 1.1
    Финальная сборка

--- LabWork1/V2DataCollection.cs
+++ LabWork1/V2DataCollection.cs
@@ -1,9 +1,8 @@
-﻿
-//Fireface17
 using Microsoft.VisualBasic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,34 +10,66 @@ namespace LabWork1
 {
     class V2DataCollection : System.Collections.ObjectModel.ObservableCollection<V2Data>
     {
-        public Collection Coll = new();
         public new V2Data this[int index]
         {
-            get { return (V2Data)this[index]; }
+            get { return this[index]; }
         }
         public bool Contains(string ID)
         {
-            if(Coll.Contains(ID))
-                return true;
+            foreach(V2Data v in this)
+            {
+                if (v.String1 == ID) return true; 
+            }
             return false;
         }
         public bool Remove(string ID)
-        {
-            if(Coll.Contains(ID)) { Coll.Remove(ID); return true; }
-            else { return false; }
-        }
-        public bool Add (V2Data v2data)
-        {
-            if (Coll.Contains(v2data.String1) == false)
+        {   for(int i = 0; i < this.Count; i++)
             {
-                Coll.Add(v2data);
-                return true;
+                if(Contains(ID)) { base.RemoveAt(Count); return true; }
             }
-            else return false;
+            return false;
+        }
+        public new bool Add(V2Data v2data)
+        {
+            foreach (V2Data v in this)
+            {
+                if (v.String1 == v2data.String1) {  return false; }
+            }
+            base.Add(v2data);
+            return true;
         }
         public void AddDefaults()
         {
-            Coll.Add
+            V2DataList taskList = new("task1", DateTime.Now);
+            Add(taskList);
+            UniformGrid taskParam = new(1, 2, 3);
+            V2DataUGrid taskUGrid = new("task2", DateTime.Now, taskParam, F);
+            Add(taskUGrid);
+            float[] taskNodes = new float[5] { 1, 2, 3, 4, 5 };
+            V2DataNUGrid taskNUGrid = new("task3", DateTime.Now, taskNodes, F);
+            Add(taskNUGrid);
+        }
+        public override string ToString()
+        {
+            string str = "V2DataCollection";
+            foreach (V2Data v2data in this)
+            {
+                str += "\n" + v2data.ToString();
+            }
+            return str;
+        }
+        public string ToLongString(string format)
+        {
+            string str = "V2DataCollection";
+            foreach (V2Data v2data in this)
+            {
+                str += "\n" + v2data.ToLongString(format);
+            }
+            return str;
+        }
+        public static Vector2 F(float x)
+        {
+            return new Vector2(x, (float)Math.Sin(x));
         }
     }
 }
