c57587b70574abdd77ab7dd0924b11caf30eff77
Tamerlan <103945044+Fireface17@users.noreply.github.com>
2022-10-28 22:22:03 +0600

    Program 1.1
    Финальная сборка

--- LabWork1/Program.cs
+++ LabWork1/Program.cs
@@ -1,5 +1,3 @@
-﻿
-//Fireface17
 using System;
 using System.Numerics;
 
@@ -11,30 +9,44 @@ namespace LabWork1
         static void Main(string[] args)
         {
             DateTime dateTime = DateTime.Now;
-            UniformGrid side = new UniformGrid(3.34f, 1, 10);
-            DataItem Vector = new DataItem(10, 12.43435f, 8.13523f);
-            Console.WriteLine(side.ToLongString("f2"));
-            Console.WriteLine(Vector.ToLongString("f2"));
-            Console.WriteLine(side.MaxNode);
-            V2DataList list = new V2DataList("Privet", dateTime);
-            list.AddDefaults(3, F);
-            Console.WriteLine(list.ToLongString("f2"));
-            V2DataList Listok = new V2DataList("Poka", dateTime);
-            Listok.AddDefaults(34, F);
-            Console.WriteLine(Listok.ToString());
-            UniformGrid test = new(0, 1, 10);
-            Console.WriteLine(test.ToString());
-            V2DataUGrid Gridd = new("privetttt", dateTime, test, F);
-            float min = Gridd.MinDistance;
-            Console.WriteLine(Gridd.ToString());
-            Console.WriteLine(Gridd.ToLongString("f2"));
-            UniformGrid Vova = new(0, 1, 250);
-            V2DataUGrid Vovanstrr = new("haha", dateTime, Vova, F);
-            Console.WriteLine(Vovanstrr.ToLongString("f4"));
-            float[] Nudes = new float[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-            V2DataNUGrid NGrid = new("haha", dateTime, Nudes, F);
-            Console.WriteLine(NGrid.ToLongString("f4"));
-            Console.WriteLine(NGrid.ToString());
+            //UniformGrid side = new UniformGrid(3.34f, 1, 10);
+            //DataItem Vector = new DataItem(10, 12.43435f, 8.13523f);
+            //Console.WriteLine(side.ToLongString("f2"));
+            //Console.WriteLine(Vector.ToLongString("f2"));
+            //Console.WriteLine(side.MaxNode);
+            //V2DataList list = new V2DataList("Privet", dateTime);
+            //list.AddDefaults(3, F);
+            //Console.WriteLine(list.ToLongString("f2"));
+            //V2DataList Listok = new V2DataList("Poka", dateTime);
+            //Listok.AddDefaults(34, F);
+            //Console.WriteLine(Listok.ToString());
+            //UniformGrid test = new(0, 1, 10);
+            //Console.WriteLine(test.ToString());
+            //V2DataUGrid Gridd = new("privetttt", dateTime, test, F);
+            //float min = Gridd.MinDistance;
+            //Console.WriteLine(Gridd.ToString());
+            //Console.WriteLine(Gridd.ToLongString("f2"));
+            //UniformGrid Vova = new(0, 1, 250);
+            //V2DataUGrid Vovanstrr = new("haha", dateTime, Vova, F);
+            //Console.WriteLine(Vovanstrr.ToLongString("f4"));
+            //float[] Nudes = new float[10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            //V2DataNUGrid NGrid = new("haha", dateTime, Nudes, F);
+            //Console.WriteLine(NGrid.ToLongString("f4"));
+            //Console.WriteLine(NGrid.ToString());
+            // ---------------------------------main task----------------------------------------
+            UniformGrid TaskGrid = new(2, 3, 6);
+            V2DataUGrid Task = new("Task", dateTime, TaskGrid, F);
+            Console.WriteLine(Task.ToLongString("f2"));
+            V2DataList NewList = (V2DataList)Task;
+            Console.WriteLine(NewList.ToLongString("f2"));
+            V2DataCollection TaskColl = new();
+            TaskColl.AddDefaults();
+            Console.WriteLine(TaskColl.ToLongString("f2"));
+            Console.WriteLine("MinDistance\n");
+            foreach(V2Data v2data in TaskColl)
+            {
+                Console.WriteLine(v2data.MinDistance);
+            }
         }
         public static Vector2 F(float x)
         {
